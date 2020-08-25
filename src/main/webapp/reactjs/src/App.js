52f00092bf281cd973314b9559a4448c4e3ac389
Helen_Shmelevski <alen.zahodyackina@yandex.ru>
2020-08-26 02:04:12 +0400

    Refactoring

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -14,8 +14,6 @@ import Edit from '@material-ui/icons/Edit';
 import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
 import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 import axios from 'axios'
@@ -33,7 +31,6 @@ const tableIcons = {
     Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
     DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
     Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
     Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
     FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
     LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
@@ -42,41 +39,19 @@ const tableIcons = {
     ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
     Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
     SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
     ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
 };
-
-const COLORS = [
-    'rgba(658, 118, 11, 1)',
-    'rgba(12, 0, 358, 1)',
-    'rgba(20, 92, 40, 1)',
-    'rgba(20, 250, 68, 1)',
-    'rgba(235, 115, 135, 1)',
-    'rgba(92, 26, 20, 1)',
-    'rgba(62, 46, 110, 1)',
-    'rgba(230, 87, 222, 1)',
-    'rgba(151, 87, 230, 1)',
-    'rgba(185, 230, 87, 1)',
-    'rgba(230, 189, 87, 1)',
-    'rgba(87, 230, 192, 1)',
-    'rgba(84, 153, 91, 1)',
-    'rgba(118, 62, 163, 1)',
-    'rgba(252, 68, 68, 1)',
-    'rgba(54, 255, 195, 1)'
-];
-
 
 function App() {
 
     let columns = [
         {title: "id", field: "id", hidden: true},
-        {title: "Дата", field: "date", placeholder: 'Введите дату в формате гггг-мм-дд'},
-        {title: "Компания", field: "company", placeholder: 'Введите название компании'},
-        {title: "Цена", field: "cost", placeholder: 'Введите сумму'}
+        {title: "Дата", field: "date", type: 'date'},
+        {title: "Компания", field: "company"},
+        {title: "Цена", field: "cost", type: 'numeric'}
     ];
-    const [data, setData] = useState([]); //table data
 
-//for error handling
+    const [data, setData] = useState([]); //table data
     const [iserror, setIserror] = useState(false);
     const [errorMessages, setErrorMessages] = useState([]);
     const [chart, setChart] = useState({
@@ -130,45 +105,40 @@ function App() {
                 setChart(newChart)
             })
             .catch(error => {
+                setErrorMessages(["Ошибка загрузки данных!"]);
+                setIserror(true);
             })
     };
 
 
     const validPeriod = (start, end, dateString) => {
-        if (start <= dateString && dateString <= end) {
-            return true;
-        } else
-            return false
+        return start <= dateString && dateString <= end;
     };
 
     const isValidDate = (dateString) => {
-        let start = new Date("1556-01-01");
-        let end = new Date();
-        let regEx = /^\d{4}-\d{2}-\d{2}$/;
-        if (!dateString.match(regEx)) return false;  // Invalid format
-        let toDate = new Date(dateString);
-        if (!validPeriod(start, end, toDate)) return false;
-        let dNum = toDate.getTime();
-        if (!dNum && dNum !== 0) return false; // NaN value, Invalid date
-        return toDate.toISOString().slice(0, 10) === dateString;
+        return validPeriod(new Date("1556-01-01"), new Date(), new Date(dateString));
     };
 
     useEffect(() => {
         loadData();
     }, []);
 
-    const handleRowUpdate = (newData, oldData, resolve) => {
-        //validation
+    const validateData = (data) => {
         let errorList = [];
-        if (newData.date === "" || !isValidDate(newData.date)) {
+        if (data.date === "" || !isValidDate(data.date)) {
             errorList.push("Ошибка! Не верное значение даты. Попробуйте еще раз")
         }
-        if (newData.company === "") {
+        if (data.company === "") {
             errorList.push("Ошибка! Отсутствует название компании. Попробуйте еще раз")
         }
-        if (newData.cost === "" || isNaN(newData.cost) || newData.cost <= 0) {
+        if (data.cost === "" || data.cost <= 0) {
             errorList.push("Ошибка! Не верное значение цены. Попробуйте еще раз")
         }
+        return errorList;
+    }
+
+    const handleRowUpdate = (newData, oldData, resolve) => {
+        let errorList = validateData(newData);
 
         if (errorList.length < 1) {
             api.patch("/update/" + newData.id, newData)
@@ -196,17 +166,7 @@ function App() {
     };
 
     const handleRowAdd = (newData, resolve) => {
-        //validation
-        let errorList = [];
-        if (newData.date === undefined || !isValidDate(newData.date)) {
-            errorList.push("Ошибка! Не верное значение даты. Попробуйте еще раз")
-        }
-        if (newData.company === undefined) {
-            errorList.push("Ошибка! Отсутствует название компании. Попробуйте еще раз")
-        }
-        if (newData.cost === undefined || isNaN(newData.cost) || newData.cost <= 0) {
-            errorList.push("Ошибка! Не верное значение цены. Попробуйте еще раз")
-        }
+        let errorList = validateData(newData);
 
         if (errorList.length < 1) { //no error
             api.post("/create", newData)
@@ -222,7 +182,7 @@ function App() {
                 .catch(error => {
                     setErrorMessages(["Ошибка создания новой транзакции!"]);
                     setIserror(true);
-                    resolve()
+                    // resolve()
                 })
         } else {
             setErrorMessages(errorList);
@@ -232,8 +192,6 @@ function App() {
     };
 
     const handleRowDelete = (oldData, resolve) => {
-
-        console.log(oldData);
         api.delete("/delete/" + oldData.id)
             .then(res => {
                 const dataDelete = [...data];
